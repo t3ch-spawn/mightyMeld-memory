b6ff54104419ffdff754a9785f1069dbc123f5b7
Tife <boluwatife1904@gmail.com>
2024-02-26 02:03:13 +0100

    added page transition

---
+++ src/Transition.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import gsap from "gsap";
+
+export function runTransition(color) {
+  if (color == "pink") {
+    gsap
+      .timeline()
+      .to(".pink-bg", {
+        scaleY: 100,
+        ease: "power2.in",
+        transformOrigin: "bottom",
+        duration: 0.5,
+      })
+      .to(".pink-bg", {
+        scaleY: 0,
+        ease: "power3.inOut",
+        transformOrigin: "top",
+        duration: 0.7,
+      });
+  } else if (color == "blue") {
+    gsap
+      .timeline()
+      .to(".blue-bg", {
+        scaleY: 100,
+        ease: "power2.in",
+        transformOrigin: "bottom",
+        duration: 0.5,
+      })
+      .to(".blue-bg", {
+        scaleY: 0,
+        ease: "power3.out",
+        transformOrigin: "top",
+        duration: 0.7,
+      });
+  }
+}
+
+export function Transition() {
+  return (
+    <>
+      <div className="blue-bg fixed w-full h-full top-0 left-0 bg-[#595BEF] z-[200] scale-y-0 pointer-events-none"></div>
+      <div className="pink-bg fixed w-full h-full top-0 left-0 bg-[#E4458F] z-[200] scale-y-0 pointer-events-none"></div>
+    </>
+  );
+}
